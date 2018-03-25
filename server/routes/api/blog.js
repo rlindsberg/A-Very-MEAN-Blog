30213146502a3eb65424c94f55d05877cfe12d1e
rlindsberg <roderick@karlemstrand.com>
2018-03-25 06:47:07 +0000

    Add mongoDB

--- server/routes/api/blog.js
+++ server/routes/api/blog.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost:27017', (err, db) => {
+        if (err) return console.log(err);
+
+        closure(db);
+    });
+};
+
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+let response = {
+    status: 200,
+    posts: [],
+    message: null
+};
 
 /* GET blog listing. */
 router.get('/', (req, res) => {
