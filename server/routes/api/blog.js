694694c05f6bf42553415037fe1c8ca41c64d120
rlindsberg <roderick@karlemstrand.com>
2018-03-25 07:21:19 +0000

    Update blog.js routes

--- server/routes/api/blog.js
+++ server/routes/api/blog.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
+
 const connection = (closure) => {
     return MongoClient.connect('mongodb://localhost:27017', (err, db) => {
         if (err) return console.log(err);
@@ -25,27 +26,90 @@ let response = {
 
 /* GET blog listing. */
 router.get('/', (req, res) => {
-    res.json(['get']);
+    connection((db) => {
+        db.collection('blog')
+            .find()
+            .sort({created_at: -1}).toArray()
+            .then((posts) => {
+                response.posts = posts;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
 });
 
 /* GET a given post */
 router.get('/:postId/', (req, res) => {
-    res.json(['getPostID']);
+    connection((db) => {
+        const id = new ObjectID(req.params.postId);
+
+        db.collection('blog').findOne({_id: id})
+            .then((post) => {
+                response.posts = [post];
+                res.json(response);
+            })
+            .catch(err => {
+                sendError(err, res);
+            })
+    });
 });
 
 /* POST a new post */
 router.post('/', (req, res) => {
-    res.json(['post']);
+    connection((db) => {
+        let post = req.body;
+        post.created_at = new Date();
+        post.updated_at = null;
+        post.comments = [];
+
+        db.collection('blog').insert(post)
+            .then((data) => {
+                db.collection('blog').findOne({_id: data.insertedIds[0]})
+                    .then((item) => {
+                        response.posts = [item];
+                        res.json(response);
+                    });
+
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
 });
 
 /* UPDATE a post */
 router.put('/:postId/', (req, res) => {
-    res.json(['put']);
+    connection((db) => {
+        const id = new ObjectID(req.params.postId);
+        let post = req.body;
+        post.updated_at = new Date();
+
+        db.collection('blog').update({_id: id}, post)
+            .then((result) => {
+                db.collection('blog').findOne({_id: id})
+                    .then((item) => {
+                        response.posts = [item];
+                        res.json(response);
+                    });
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
 });
 
 /* DELETE a post */
 router.delete('/:postId/', (req, res) => {
-    res.json(['del']);
+    connection(db => {
+        const id = new ObjectID(req.params.postId);
+        db.collection('blog').deleteOne({_id: id})
+            .then(data => {
+                res.json(response);
+            })
+            .catch(err => sendError(err, res));
+    });
 });
 
 module.exports = router;
