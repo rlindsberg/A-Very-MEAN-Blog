2eaafdf486bd20b8b40cab2f051377590251c6fe
rlindsberg <roderick@karlemstrand.com>
2018-05-24 20:25:56 +0200

    Add route for user page. Change debug command

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+// GET user page
+router.get('/user', function(req, res, next) {
+  res.send('Welcome to the user page!' );
+});
 module.exports = router;
