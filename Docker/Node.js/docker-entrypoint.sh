ff8c8e47da35f3dc0f6e041433bcf8690ddb7b4b
rlindsberg <roderick@karlemstrand.com>
2019-05-23 13:16:33 +0200

    Add packages from centos 7 /usr/share/info for Docker build

--- Docker/Node.js/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo >&2 'sh running'
-
-wget https://lastmile.gomez.com/PEERInstall.exe -O /root/PEERInstall.exe
--- README.md
+++
@@ -1,2 +0,0 @@
-# A-Very-MEAN-Blog
-Professional website and blog built with M(ongoDB)E(xpress)A(ngular)N(ode.js)
